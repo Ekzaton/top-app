2fc7b07c40112c37c9d51a801e5005d49d6d0f97
Sergey Zuev <ekzaton@gmail.com>
2021-10-16 18:51:07 +0500

    5. Configured .eslintrc.json and fixed *.tsx files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import {AppProps} from "next/dist/shared/lib/router/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp

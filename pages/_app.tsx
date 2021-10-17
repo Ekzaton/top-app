167db88bab99364f94d66c4dbafef26f1d6eb74c
Sergey Zuev <ekzaton@gmail.com>
2021-10-17 12:55:14 +0500

    9. Added Head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import {AppProps} from "next/dist/shared/lib/router/router";
 
+import Head from "next/head";
+
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My Top - наш лучший топ</title>
+        <link rel="icon" href={"/favicon.ico"} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

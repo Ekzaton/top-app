bf5545b6d73002c962c9c683ae4bdf4c462fa768
Sergey Zuev <ekzaton@gmail.com>
2021-10-23 00:28:08 +0500

    28. Added withLayout HOC

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,3 +1,5 @@
+import {FunctionComponent} from "react";
+
 import {Footer} from "./Footer/Footer";
 import {Header} from "./Header/Header";
 import {Sidebar} from "./Sidebar/Sidebar";
@@ -5,7 +7,7 @@ import {Sidebar} from "./Sidebar/Sidebar";
 import styles from "./Layout.module.css";
 import {LayoutProps} from "./Layout.props";
 
-export function Layout(props: LayoutProps): JSX.Element {
+function Layout(props: LayoutProps): JSX.Element {
   const {children} = props;
 
   return (
@@ -18,4 +20,14 @@ export function Layout(props: LayoutProps): JSX.Element {
       <Footer/>
     </>
   );
+}
+
+export function withLayout<T extends Record<string, unknown>>(Component: FunctionComponent<T>) {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props}/>
+      </Layout>
+    );
+  };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
 import React, {useState} from "react";
 
 import {Button, Heading, Paragraph, Rating, Tag} from "../components";
-import {Layout} from "../layout/Layout";
+import {withLayout} from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
-      <Layout>
-        <Heading size="h3">Маленький заголовок</Heading>
-        <Heading size="h2">Средний заголовок</Heading>
-        <Heading size="h1">Большой заголовок</Heading>
-        <Button appearance="primary" arrow="right">Кнопка</Button>
-        <Button appearance="ghost" arrow="right">Кнопка</Button>
-        <Paragraph size="s">Маленький</Paragraph>
-        <Paragraph>Средний</Paragraph>
-        <Paragraph size="l">Большой</Paragraph>
-        <Tag size="s">Ghost</Tag>
-        <Tag size="m" color="red">Red</Tag>
-        <Tag size="s" color="green">Green</Tag>
-        <Tag color="primary">Primary</Tag>
-        <Rating rating={rating} setRating={setRating} isEditable />
-      </Layout>
+    <>
+      <Heading size="h3">Маленький заголовок</Heading>
+      <Heading size="h2">Средний заголовок</Heading>
+      <Heading size="h1">Большой заголовок</Heading>
+      <Button appearance="primary" arrow="right">Кнопка</Button>
+      <Button appearance="ghost" arrow="right">Кнопка</Button>
+      <Paragraph size="s">Маленький</Paragraph>
+      <Paragraph>Средний</Paragraph>
+      <Paragraph size="l">Большой</Paragraph>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">Red</Tag>
+      <Tag size="s" color="green">Green</Tag>
+      <Tag color="primary">Primary</Tag>
+      <Rating rating={rating} setRating={setRating} isEditable />
+    </>
   );
 }
+
+export default withLayout(Home);

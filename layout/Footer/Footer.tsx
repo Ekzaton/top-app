bb17a20fce73ee9fe92211af9d85b25ca3ff77cc
Sergey Zuev <ekzaton@gmail.com>
2021-10-23 23:58:47 +0500

    31. Added styles for footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
+import cn from "classnames";
+import { format } from "date-fns";
+
 import styles from "./Footer.module.css";
 import {FooterProps} from "./Footer.props";
 
 export function Footer(props: FooterProps): JSX.Element {
+  const {className, ...otherProps} = props;
+
   return (
-    <div { ...props}>
-      Footer
-    </div>
+    <footer className={cn(className, styles.footer)} { ...otherProps}>
+      <div>OwlTop © 2020 - {format(new Date, 'yyyy')} Все права защищены</div>
+      <a href="#">Пользовательское соглашение</a>
+      <a href="#">Политика конфиденциальности</a>
+    </footer>
   );
 }

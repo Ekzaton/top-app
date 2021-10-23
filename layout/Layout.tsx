26ae965c3f2cce6cc7dcff07001cf8d8b17a627b
Sergey Zuev <ekzaton@gmail.com>
2021-10-23 16:03:42 +0500

    29. Added styles for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -11,14 +11,12 @@ function Layout(props: LayoutProps): JSX.Element {
   const {children} = props;
 
   return (
-    <>
-      <Header/>
-      <div>
-        <Sidebar/>
-        <div>{children}</div>
-      </div>
-      <Footer/>
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header}/>
+      <Sidebar className={styles.sidebar}/>
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer}/>
+    </div>
   );
 }
 

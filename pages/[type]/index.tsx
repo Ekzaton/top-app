141d35c1492019591f7c5aaeb49c2dda6b58e5fe
Sergey Zuev <ekzaton@gmail.com>
2021-11-01 00:17:14 +0500

    45. Added courses index page

---
+++ pages/[type]/index.tsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
+import React from "react";
+
+import {withLayout} from "../../layout/Layout";
+import {MenuItem} from "../../interfaces/menu.interface";
+import {firstLevelMenu} from "../../helpers/helpers";
+
+interface TypeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
+
+function Type(props: TypeProps): JSX.Element {
+  const {firstCategory} = props;
+
+  return (
+      <>Type: {firstCategory}</>
+  );
+}
+
+export default withLayout(Type);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: firstLevelMenu.map(m => "/" + m.route),
+    fallback: true
+  };
+};
+
+export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }: GetStaticPropsContext) => {
+  if (!params) {
+    return {
+      notFound: true
+    };
+  }
+
+  const firstCategoryItem = firstLevelMenu.find(m => m.route === params.type);
+
+  if (!firstCategoryItem) {
+    return {
+      notFound: true
+    };
+  }
+
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory: firstCategoryItem.id
+  });
+
+  return {
+    props: {
+      menu,
+      firstCategory: firstCategoryItem.id
+    }
+  };
+};

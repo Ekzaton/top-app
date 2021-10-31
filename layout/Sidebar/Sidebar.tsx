b59379e9df55775ffd348e71c13a908f406f2afa
Sergey Zuev <ekzaton@gmail.com>
2021-11-01 00:53:09 +0500

    46. Added styles for sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,19 @@
+import cn from "classnames";
+
 import {Menu} from "../Menu/Menu";
+
+import Logo from "../logo.svg";
 
 import styles from "./Sidebar.module.css";
 import {SidebarProps} from "./Sidebar.props";
 
 export function Sidebar(props: SidebarProps): JSX.Element {
+  const {className, ...otherProps} = props;
+
   return (
-    <div { ...props}>
+    <div className={cn(className, styles.sidebar)} { ...otherProps}>
+      <Logo className={styles.logo}/>
+      <div>поиск</div>
       <Menu/>
     </div>
   );

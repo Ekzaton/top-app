e7f6228e751cd5966fb3997416deb96df0bee746
Sergey Zuev <ekzaton@gmail.com>
2021-11-20 22:36:12 +0500

    55. Updated TopPageComponent

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import {Heading, HHData, Tag} from "../../components";
+import {Advantages, Heading, HHData, Paragraph, Tag} from "../../components";
 
 import styles from "./TopPagecComponent.module.css";
 import {TopPageComponentsProps} from "./TopPageComponent.props";
@@ -21,7 +21,14 @@ export function TopPageComponent(props: TopPageComponentsProps): JSX.Element {
         <Heading size="h2">Вакансии - {page.category}</Heading>
         <Tag color="red" size="m">hh.ru</Tag>
       </div>
-      {firstCategory === TopLevelCategory.Courses && <HHData {...page.hh}/>}
+      {firstCategory === TopLevelCategory.Courses && page.hh && <HHData {...page.hh}/>}
+      {page.advantages && !!page.advantages.length && <>
+        <Heading size="h2">Преимущества</Heading>
+        <Advantages advantages={page.advantages}/>
+      </>}
+      {page.seoText && <Paragraph>{page.seoText}</Paragraph>}
+      <Heading size="h2">Получаемые навыки</Heading>
+      {page.tags.map(t => <Tag key={t} color="primary">{t}</Tag>)}
     </div>
   );
 }

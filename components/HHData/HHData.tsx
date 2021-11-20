db19d432adae1ee498f701cad03c1dd0d5644a7f
Sergey Zuev <ekzaton@gmail.com>
2021-11-20 20:37:28 +0500

    52. Added helper for price

--- components/HHData/HHData.tsx
+++ components/HHData/HHData.tsx
@@ -1,8 +1,11 @@
+import {priceRu} from "../../helpers/helpers";
+
 import {Card} from "../Card/Card";
 
 import styles from "./HHData.module.css";
 import {HHDataProps} from "./HHData.props";
 import RateIcon from "./rate.svg";
+
 
 export function HHData(props: HHDataProps): JSX.Element {
   const {count, juniorSalary, middleSalary, seniorSalary} = props;
@@ -16,7 +19,7 @@ export function HHData(props: HHDataProps): JSX.Element {
         <Card className={styles.salary}>
           <div>
             <div className={styles.title}>Начальный</div>
-            <div className={styles.salaryValue}>{juniorSalary}</div>
+            <div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
             <div className={styles.rate}>
               <RateIcon className={styles.filled}/>
               <RateIcon />
@@ -25,7 +28,7 @@ export function HHData(props: HHDataProps): JSX.Element {
           </div>
           <div>
             <div className={styles.title}>Средний</div>
-            <div className={styles.salaryValue}>{middleSalary}</div>
+            <div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
             <div className={styles.rate}>
               <RateIcon className={styles.filled}/>
               <RateIcon className={styles.filled}/>
@@ -34,7 +37,7 @@ export function HHData(props: HHDataProps): JSX.Element {
           </div>
           <div>
             <div className={styles.title}>Профессионал</div>
-            <div className={styles.salaryValue}>{seniorSalary}</div>
+            <div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
             <div className={styles.rate}>
               <RateIcon className={styles.filled}/>
               <RateIcon className={styles.filled}/>

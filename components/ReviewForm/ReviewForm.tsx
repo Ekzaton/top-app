33e862610775bb1f34ae075171d45911f8d6e8b0
Sergey Zuev <ekzaton@gmail.com>
2021-12-11 14:05:11 +0500

    77. Updated ReviewForm component

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,6 +1,8 @@
 import cn from "classnames";
+import {useForm, Controller} from "react-hook-form";
 
 import CloseIcon from "./close.svg";
+import {IReviewForm} from "./ReviewForm.interface";
 import styles from "./ReviewForm.module.css";
 import {ReviewFormProps} from "./ReviewForm.props";
 
@@ -11,17 +13,26 @@ import {Textarea} from "../Textarea/Textarea";
 
 export function ReviewForm(props: ReviewFormProps): JSX.Element {
   const {productId, className, ...otherProps} = props;
+  const {register, control, handleSubmit} = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
 
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...otherProps}>
-        <Input placeholder="Имя"/>
-        <Input className={styles.title} placeholder="Заголовок отзыва"/>
+        <Input placeholder="Имя" {...register("name")}/>
+        <Input className={styles.title} placeholder="Заголовок отзыва" {...register("title")}/>
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0}/>
+          <Controller
+            control={control}
+            name="rating"
+            render={({field}) => <Rating rating={field.value} setRating={field.onChange} isEditable/>}
+          />
         </div>
-        <Textarea className={styles.description} placeholder="Текст отзыва"/>
+        <Textarea className={styles.description} placeholder="Текст отзыва" {...register("description")}/>
         <div className={styles.submit}>
           <Button appearance="primary">Отправить</Button>
           <span>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
@@ -32,6 +43,6 @@ export function ReviewForm(props: ReviewFormProps): JSX.Element {
         <div>Спасибо, ваш отзыв будет опубликован после проверки</div>
         <CloseIcon className={styles.close}/>
       </div>
-    </>
+    </form>
   );
 }

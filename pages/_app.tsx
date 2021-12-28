4a24d7f469f72212037369750ee41a568f94a8b7
Sergey Zuev <ekzaton@gmail.com>
2021-12-29 00:36:13 +0500

    107. Connected yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import {AppProps} from "next/dist/shared/lib/router/router";
 import Head from "next/head";
+import ym, {YMInitializer} from "react-yandex-metrika";
 
 import "../styles/globals.css";
 
 export default function MyApp(props: AppProps): JSX.Element {
   const {Component, pageProps, router} = props;
+
+  if (typeof window !== "undefined") {
+    router.events.on("routeChangeComplete", (url: string) => ym("hit", url));
+  }
+
 
   return (
     <>
@@ -13,10 +19,12 @@ export default function MyApp(props: AppProps): JSX.Element {
         <link rel="icon" href={"/favicon.ico"} />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link rel="preconnect" href="https://mc.yandex.ru"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
         <meta property="og:locale" content="ru_RU"/>
       </Head>
+      <YMInitializer accounts={[]} options={{webvisor: true, defer: true}} version="2"/>
       <Component {...pageProps} />
     </>
   );
